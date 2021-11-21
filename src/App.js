ff290402eae153eeaec937ad9ca2c64b57eaa067
Karina Finegan <kairosity@users.noreply.github.com>
2021-11-21 17:31:27 +0000

    feat: add Navbar & Sidebar

--- src/App.js
+++ src/App.js
@@ -5,24 +5,30 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css'
 
 // pages & components
+import Navbar from './components/Navbar';
 import Dashboard from './pages/dashboard/Dashboard'
 import Create from './pages/create/Create'
 import Login from './pages/login/Login'
 import Project from './pages/project/Project'
 import Signup from './pages/signup/Signup'
+import Sidebar from './components/Sidebar';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Dashboard />} />
-          <Route path='/create' element={<Create />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/project' element={<Project />} />
-          <Route path='/signup' element={<Signup />} />
-        </Routes>
+        <Sidebar />
+        <div className="container">
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/create' element={<Create />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/project' element={<Project />} />
+            <Route path='/signup' element={<Signup />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
