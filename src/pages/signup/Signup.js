1abd0442048c80e488854e35d71959784194cca7
Karina Finegan <kairosity@users.noreply.github.com>
2021-11-22 13:58:39 +0000

    feat: photo upload to firestorage

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useSignup } from '../../hooks/useSignup'
 
 // styles
 import './Signup.css'
@@ -10,10 +11,11 @@ export default function Signup() {
     const [displayName, setDisplayName] = useState('')
     const [thumbnail, setThumbnail] = useState(null)
     const [thumbnailError, setThumbnailError] = useState(null)
+    const {signup, isPending, error} = useSignup()
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(email, password, displayName, thumbnail)
+        signup(email, password, displayName, thumbnail)
     }
 
     // func to handle avatar image uploads
@@ -89,7 +91,9 @@ export default function Signup() {
                 />
                 {thumbnailError && <div className="error">{thumbnailError}</div>}
             </label>
-            <button className='btn'>Sign Up</button>
+            {!isPending && <button className='btn'>Sign Up</button>}
+            {isPending && <button className='btn' disabled>Loading...</button>}
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
