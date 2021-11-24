2305edd442248a6f8fbb2b82c4bf5b310d80c2f4
Karina Finegan <kairosity@users.noreply.github.com>
2021-11-24 19:06:24 +0000

    feat: most main app features completed

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import { useCollection } from '../../hooks/useCollection'
 import { useState } from 'react'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 // styles
 import './Dashboard.css'
@@ -8,21 +9,46 @@ import './Dashboard.css'
 import ProjectList from '../../components/ProjectList'
 import ProjectFilter from './ProjectFilter'
 
+
 export default function Dashboard() {
 
     const [currentFilter, setCurrentFilter] = useState('all')
     const { documents, error } = useCollection('projects')
+    const { user } = useAuthContext()
 
     const changeFilter = (newFilter) => {
         setCurrentFilter(newFilter)
     }
+
+    const projects = documents ? documents.filter((document) => {
+        switch(currentFilter){
+            case 'all':
+                return true
+            case 'mine':
+                let assignedToMe = false
+                document.assignedUsersList.forEach((u) => {
+                    if(u.uid === user.uid){
+                        assignedToMe = true
+                    }
+                })
+                return assignedToMe
+            case 'development':
+            case 'design':
+            case 'sales':
+            case 'marketing':
+                console.log(document.category, currentFilter)
+                return document.category === currentFilter
+            default:
+                return true
+        }
+    }) : null
 
     return (
         <div>
             <h2 className='page-title'>Dashboard</h2>
             {error && <p className='error'>{error}</p>}
             {documents && <ProjectFilter currentFilter={currentFilter} changeFilter={changeFilter} />}
-            {documents && <ProjectList projects={documents} /> }
+            {projects && <ProjectList projects={projects} /> }
         </div>
     )
 }
